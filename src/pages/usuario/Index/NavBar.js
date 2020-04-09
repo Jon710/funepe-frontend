d73e6d3b7bc00575dafcd1fcdbb41a95da577baa
moraes65 <moraes.jlcm@gmail.com>
2020-04-09 02:07:17 -0300

    Create funepe-frontend06.zip

--- src/pages/usuario/Index/NavBar.js
+++ src/pages/usuario/Index/NavBar.js
@@ -4,8 +4,9 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Nav } from 'react-bootstrap';
 
 import Notifications from '~/pages/usuario/Notifications';
-import { signOutUser } from '~/redux/features/usuario/usuarioSlice';
 // import Paciente from '~/pages/usuario/Index/Paciente';
+import { signOutUser } from '~/redux/features/usuario/usuarioSlice';
+
 import logo from '~/assets/logo.jpg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../../styles/vendor/fontawesome-free/css/all.min.css';
