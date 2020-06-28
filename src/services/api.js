4f3cfb5eec318e5eb590e15115ec125ebc0f2291
João Luis Moraes <joaoluismoraes@Joaos-MacBook-Pro-2.local>
2020-06-27 21:04:06 -0300

    fix cors

--- src/services/api.js
+++ src/services/api.js
@@ -2,13 +2,14 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-  // headers: {
-  //   'Access-Control-Allow-Origin': '*',
-  // },
+  headers: { 'Access-Control-Allow-Origin': '*' },
 });
+// .then(request => {
+//   return request.headers['access-control-allow-origin'];
+// });
 
-api.defaults.headers.post['Access-Control-Allow-Origin'] =
-  'https://funepe-backend.herokuapp.com';
+// api.defaults.headers.post['Access-Control-Allow-Origin'] =
+//   'https://funepe-backend.herokuapp.com';
 
 // api.defaults.headers.common.Authorization = `Bearer ${
 //   store.getState().usuario.token
