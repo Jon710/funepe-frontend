b480c6d6ea9ec3077d2c09a4bcbaffe368387aa1
João Luis Moraes <joaoluismoraes@Joaos-MacBook-Pro-2.local>
2020-06-27 23:06:15 -0300

    fix cors

--- src/services/api.js
+++ src/services/api.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-  // headers: {
-  //   'X-Requested-With': 'XMLHttpRequest',
-  //   'Content-Type': 'application/x-www-form-urlencoded',
-  // },
+  headers: {
+    'X-Requested-With': 'XMLHttpRequest',
+    // 'Content-Type': 'application/x-www-form-urlencoded',
+  },
 });
 // axios.defaults.headers.post['Content-Type'] =
 //   'application/x-www-form-urlencoded';
