ccc4448227f236491c422b637c8ca6705016d58f
João Luis Moraes <joaoluismoraes@Joaos-MacBook-Pro-2.local>
2020-06-27 23:00:47 -0300

    fix cors

--- src/services/api.js
+++ src/services/api.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-  headers: {
-    'X-Requested-With': 'XMLHttpRequest',
-    'Content-Type': 'application/x-www-form-urlencoded',
-  },
+  // headers: {
+  //   'X-Requested-With': 'XMLHttpRequest',
+  //   'Content-Type': 'application/x-www-form-urlencoded',
+  // },
 });
-axios.defaults.headers.post['Content-Type'] =
-  'application/x-www-form-urlencoded';
+// axios.defaults.headers.post['Content-Type'] =
+//   'application/x-www-form-urlencoded';
 
 // .then(request => {
 //   return request.headers['access-control-allow-origin'];
